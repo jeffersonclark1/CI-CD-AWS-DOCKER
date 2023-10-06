8c8908e38079edeb4a1cb190c560b597380bb784
Jefferson Clark <jeffersonclarkti@gmail.com>
2023-10-06 15:08:44 -0300

    Update file of service

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD APIs Vayon");
+  res.send("CI/CD Service Vayon");
 });
 
 app.get("/info", (req, res) => {
